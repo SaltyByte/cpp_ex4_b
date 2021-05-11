dae17b5bbc8f3ed3a8318cb65c98b473ff276dfa
Alex <alexnova98@gmail.com>
2021-05-11 12:15:13 +0300

    adding to github

---
+++ Demo.cpp
@@ -0,0 +1,36 @@
+/**
+ * Demo program for Pandemic exercise
+ * 
+ * Author: Alex Baranov
+ */
+
+#include "sources/Board.hpp"
+#include "sources/City.hpp"
+#include "sources/Color.hpp"
+#include "sources/Player.hpp"
+
+#include "sources/OperationsExpert.hpp"
+#include "sources/Dispatcher.hpp"
+#include "sources/FieldDoctor.hpp"
+#include "sources/Medic.hpp"
+#include "sources/Researcher.hpp"
+#include "sources/Scientist.hpp"
+#include "sources/GeneSplicer.hpp"
+#include "sources/Virologist.hpp"
+
+using namespace pandemic;
+
+
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
+
